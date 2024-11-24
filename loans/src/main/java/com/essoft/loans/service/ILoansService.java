3cdce6b69761fcfc905155f5309b62ae056e1998
ErtanSidar <essoft3@gmail.com>
2024-11-24 10:28:13 +0300

    add loan service

---
+++ loans/src/main/java/com/essoft/loans/service/ILoansService.java
@@ -0,0 +1,15 @@
+package com.essoft.loans.service;
+
+import com.essoft.loans.dto.LoansDto;
+
+public interface ILoansService {
+
+    void createLoan(String mobileNumber);
+
+    LoansDto fetchLoan(String mobileNumber);
+
+    boolean updateLoan(LoansDto loansDto);
+
+    boolean deleteLoan(String mobileNumber);
+
+}
