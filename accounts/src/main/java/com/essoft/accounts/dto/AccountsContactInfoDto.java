67f1cd23e94d417c71b10a76da1ccf06c809d1ec
ErtanSidar <essoft3@gmail.com>
2024-11-29 23:42:33 +0300

    add remote configs(github)

--- accounts/src/main/java/com/essoft/accounts/dto/AccountsContactInfoDto.java
+++ accounts/src/main/java/com/essoft/accounts/dto/AccountsContactInfoDto.java
@@ -6,6 +6,33 @@ import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+public class AccountsContactInfoDto {
 
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Map<String, String> getContactDetails() {
+        return contactDetails;
+    }
+
+    public void setContactDetails(Map<String, String> contactDetails) {
+        this.contactDetails = contactDetails;
+    }
+
+    public List<String> getOnCallSupport() {
+        return onCallSupport;
+    }
+
+    public void setOnCallSupport(List<String> onCallSupport) {
+        this.onCallSupport = onCallSupport;
+    }
 }
