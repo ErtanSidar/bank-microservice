1a1ea876757add007feb6fd5fe9cd031d4e72861
ErtanSidar <essoft3@gmail.com>
2024-12-19 14:48:36 +0300

    add eureka server

---
+++ eurekaserver/src/test/java/com/essoft/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.essoft.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
