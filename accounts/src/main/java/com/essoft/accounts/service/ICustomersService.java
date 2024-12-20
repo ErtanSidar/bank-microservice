1a36b710675cdf9be9fa1788d4b2bac46e5230dc
ErtanSidar <essoft3@gmail.com>
2024-12-20 15:17:27 +0300

    add feign client and update docker compose

---
+++ accounts/src/main/java/com/essoft/accounts/service/ICustomersService.java
@@ -0,0 +1,8 @@
+package com.essoft.accounts.service;
+
+import com.essoft.accounts.dto.CustomerDetailsDto;
+
+public interface ICustomersService {
+
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+}
---
+++ accounts/src/main/java/com/essoft/accounts/service/impl/CustomersServiceImpl.java
@@ -0,0 +1,51 @@
+package com.essoft.accounts.service.impl;
+
+import com.essoft.accounts.client.CardsFeignClient;
+import com.essoft.accounts.client.LoansFeignClient;
+import com.essoft.accounts.dto.AccountsDto;
+import com.essoft.accounts.dto.CardsDto;
+import com.essoft.accounts.dto.CustomerDetailsDto;
+import com.essoft.accounts.dto.LoansDto;
+import com.essoft.accounts.entity.Accounts;
+import com.essoft.accounts.entity.Customer;
+import com.essoft.accounts.exception.ResourceNotFoundException;
+import com.essoft.accounts.mapper.AccountsMapper;
+import com.essoft.accounts.mapper.CustomerMapper;
+import com.essoft.accounts.repository.AccountsRepository;
+import com.essoft.accounts.repository.CustomerRepository;
+import com.essoft.accounts.service.ICustomersService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class CustomersServiceImpl implements ICustomersService {
+
+    private AccountsRepository accountsRepository;
+    private CustomerRepository customerRepository;
+    private CardsFeignClient cardsFeignClient;
+    private LoansFeignClient loansFeignClient;
+
+    @Override
+    public CustomerDetailsDto fetchCustomerDetails(String mobileNumber) {
+        Customer customer = customerRepository.findByMobileNumber(mobileNumber).orElseThrow(
+                () -> new ResourceNotFoundException("Customer", "mobileNumber", mobileNumber)
+        );
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId()).orElseThrow(
+                () -> new ResourceNotFoundException("Account", "customerId", customer.getCustomerId().toString())
+        );
+
+        CustomerDetailsDto customerDetailsDto = CustomerMapper.mapToCustomerDetailsDto(customer, new CustomerDetailsDto());
+        customerDetailsDto.setAccountsDto(AccountsMapper.mapToAccountsDto(accounts, new AccountsDto()));
+
+        ResponseEntity<LoansDto> loansDtoResponseEntity = loansFeignClient.fetchLoanDetails(mobileNumber);
+        customerDetailsDto.setLoansDto(loansDtoResponseEntity.getBody());
+
+        ResponseEntity<CardsDto> cardsDtoResponseEntity = cardsFeignClient.fetchCardDetails(mobileNumber);
+        customerDetailsDto.setCardsDto(cardsDtoResponseEntity.getBody());
+
+        return customerDetailsDto;
+
+    }
+}
