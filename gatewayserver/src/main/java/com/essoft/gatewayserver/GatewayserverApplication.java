c1b07c063f8c8a3b5cc1170c00a8551f2ba4bef6
ErtanSidar <essoft3@gmail.com>
2024-12-20 15:42:59 +0300

    add gateway server

---
+++ gatewayserver/src/main/java/com/essoft/gatewayserver/GatewayserverApplication.java
@@ -0,0 +1,13 @@
+package com.essoft.gatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayserverApplication.class, args);
+	}
+
+}
